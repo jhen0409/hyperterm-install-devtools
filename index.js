640adb1cac80d5dd785836712702020370b495a9
Jhen <developer@jhen.me>
2016-07-21 11:52:10 +0800

    Add notify

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+const { BrowserWindow } = require('electron')
 const installer = require('electron-devtools-installer')
+
+const notify = (title, body) => {
+  const win = BrowserWindow.getAllWindows()[0];
+  if (win) {
+    win.webContents.executeJavaScript(`
+      new Notification('${title}', {
+        body: '${body}'
+      })
+    `)
+  }
+}
 
 const install = (extensions, forceDownload, index = 0) => {
   let id = extensions[index]
   if (installer[id]) id = installer[id]
   if (!extensions[index]) return
 
+  const next = () => install(extensions, forceDownload, index + 1)
+
   installer.default(id, forceDownload)
-    .then(() => install(extensions, forceDownload, index + 1))
-    .catch(() => install(extensions, forceDownload, index + 1))
+    .then(next)
+    .catch(err => {
+      notify('Install extension', err)
+      next()
+    })
 }
 
 exports.onApp = app => {
